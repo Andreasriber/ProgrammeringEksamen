0bcf496e607149381bd3234a1988bd324bc18167
andreas Riber <andreasriber@gmail.com>
2022-11-24 21:47:14 +0100

    nummer5

---
+++ js/time.js
@@ -0,0 +1,30 @@
+
+
+let clockTime = document.getElementById("clockTime")
+let date = document.getElementById("date")
+
+
+// RESSOURCES brugt
+/*
+
+
+DET ANDET VAR LORT
+https://www.youtube.com/watch?v=E0UGGxd2DOo&ab_channel=dcode
+http://worldtimeapi.org/ */
+
+function liveTime(){
+    
+fetch("http://worldtimeapi.org/api/timezone/Europe/Copenhagen")
+  .then((response) => response.json())              
+  .then((data) =>{
+  
+  clockTime.innerHTML = data.datetime
+  console.log(data.datetime)
+  
+  
+});  
+  
+}
+liveTime()
+
+
