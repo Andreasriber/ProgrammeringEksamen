f83d82a14b78591121dd403d49a141d60d29a8bf
andreas Riber <andreasriber@gmail.com>
2022-12-10 00:16:34 +0100

    lastCommitThankYou

--- js/navigation.js
+++ js/navigation.js
@@ -4,7 +4,12 @@ let state = localStorage.getItem("status");
 document.getElementById("knap").innerHTML = "Log In";
 
 if(state === "active"){
-    document.getElementById("profilePicture").src = "../images/natur.jpg"
+    if (localStorage.getItem("profilePicture") === null){
+        document.getElementById("profilePicture").src = "../images/profilepicture/earth.png"
+    }else {
+        document.getElementById("profilePicture").src = localStorage.getItem("profilePicture")
+    }
+    
     document.getElementById("accountName").innerHTML = localStorage.getItem("name")
     document.getElementById("knap").innerHTML = "Log Ud";
     document.getElementById("profilePicture").addEventListener("click",(e)=>{
@@ -19,18 +24,30 @@ if(state === "active"){
     document.getElementById("profilePicture").src = "../images/userNone.png"
     document.getElementById("accountName").innerHTML = ""
 }
-  
+
 document.getElementById("knap").addEventListener("click", (e)=>{
     location.href = "../html/login.html"
 })
 
-
+if(state === "active"){
 document.getElementById("homeLogo").addEventListener("click", (e) =>{
     location.href = "../html/index.html"
 })
+} else{
+    document.getElementById("homeLogo").addEventListener("click", (e) =>{
+        alert("You must register or log-in first!")
+    })
+}
 
-
-
+if(state === "active"){
+    document.getElementById("likedLogo").addEventListener("click", (e) =>{
+        location.href = "../html/favourite.html"
+    })
+    } else{
+        document.getElementById("likedLogo").addEventListener("click", (e) =>{
+            alert("You must register or log-in first!")
+        })
+    }
 
 
 
